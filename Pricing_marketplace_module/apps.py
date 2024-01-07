880dce359c56ba803ded775a713f5ea16a6798cc
nataliagovorushkina <a10061514@gmail.com>
2024-01-07 15:27:24 +0300

    Init

---
+++ Pricing_marketplace_module/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PricingMarketplaceModuleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Pricing_marketplace_module'
